ffdeb6798ffbc0893a283f491ca4fb8675eabfb1
HyeongUk Lim <hyeonguklim@gmail.com>
2019-07-15 22:08:19 -0500

    Add description

--- ShiftedGamma_parameter.m
+++ ShiftedGamma_parameter.m
@@ -1,4 +1,10 @@
 function [k,v,gamma] = ShiftedGamma_parameter(data)
+% obtain parameters of a shifted gamma distribution by the method of moments
+
+%k = 4/skewness(data,0)^2;
+%v = sqrt(k)/std(data,0);
+%gamma = mean(data) - k/v;
+
 k = 4/skewness(data)^2;
 v = sqrt(k)/std(data);
 gamma = mean(data) - k/v;
